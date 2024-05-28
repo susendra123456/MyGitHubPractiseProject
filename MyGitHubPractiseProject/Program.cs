0a0639d23d57ee77fc95624173f0c8b37516a4a0
susendra123456 <111244965+susendra123456@users.noreply.github.com>
2024-05-28 22:08:42 +0530

    Update Program.cs
    //This is added in Github directly in Origin Branch

--- MyGitHubPractiseProject/Program.cs
+++ MyGitHubPractiseProject/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Helo welocme to My GitHub Practise roject");
+            Console.writeLine("This is one more change done by others so we can pull this version from Github to Visual studio Master Repository i.e From Origin to MAster");
         }
     }
 }

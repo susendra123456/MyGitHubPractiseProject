f05985b7480b2418fc6f14cb2d601d29f37e59e0
susendra <56603738+susendra@users.noreply.github.com>
2024-05-28 22:03:04 +0530

    Console.writeLine Modified Before Pushing your Changeds to Remote Repository u need to commit changes to Local Repository.

--- MyGitHubPractiseProject/Program.cs
+++ MyGitHubPractiseProject/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Helo welocme to My GitHub Practise roject");
         }
     }
 }
